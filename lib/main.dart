a9cc3d4e3c1b19b08e4e6b4a01d9a17540127f71
Chethana N P <chethana.np@cumulations.com>
2022-06-25 23:52:18 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
-import './app_screens/first_screen.dart';
+import './app_screens/home.dart';
 
-void main() => runApp(MyApp());
-
-
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "FlutterApp_1",
-      home: Scaffold(
-        appBar: AppBar(title: Text("My First Flutter App"),),
-        body: FirstScreen()
-      ),
-
-    );
-  }
-  
+void main(){
+  runApp
+  (MaterialApp(
+    title: "Exploring UI widgets",
+    home: Home(),
+  )
+  );
 }
