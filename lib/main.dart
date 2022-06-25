be384f7a3c433002a03269dd2367fe83ddcb21e5
Chethana N P <chethana.np@cumulations.com>
2022-06-25 20:12:16 +0530

    added scaffold class to add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,23 @@ void main() {
 
 	runApp(
 
-    new MaterialApp(
-      title: "My First Flutter App",
-      home: new Material(
-        color: Colors.amberAccent,
-        child: Center(
-			child: Text(
-				"Hello World!!",
-				textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 30.0, color: Colors.white),
-		  ),
-		),
-      )
-    )
+     MaterialApp(
+      title: "FlutterApp_1",
+      home: Scaffold(
+        appBar: AppBar(title: Text("My First Flutter App"),),
+        body: Material(
+              color: Colors.amberAccent,
+              child: Center(
+			        child: Text(
+			              	"Hello World!!",
+			              	textDirection: TextDirection.ltr,
+                      style: TextStyle(fontSize: 30.0, color: Colors.white),
+		                      ),
+	                      	),
+                      )
+                    ),
 
-		
+                  )
+  	
 	);
 }
