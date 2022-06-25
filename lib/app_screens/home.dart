b7aacdf9e54d28126eebf0729144b6a6b97cef91
Chethana N P <chethana.np@cumulations.com>
2022-06-26 00:13:52 +0530

    container widget with margin and padding

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,15 +1,21 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Center(
+      child: Container(
+        // width: 300.0,
+        // height: 100.0,
+      // margin: EdgeInsets.all(20.0),  
+      // margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 0.0, bottom: 0.0),
       alignment: Alignment.center,
-      color: Colors.white,
+      color: Color.fromARGB(255, 208, 126, 246),
       child: Text("Flight", textDirection: TextDirection.ltr),
-    );
+    ),
+    )
+    
+    ;
   }
 }
