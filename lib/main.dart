819cdbc339b1892dadd09167813664ef45253bc4
Chethana N P <chethana.np@cumulations.com>
2022-06-26 22:51:15 +0530

    merge issues

--- lib/main.dart
+++ lib/main.dart
@@ -11,37 +11,9 @@ void main(){
     title: "Exploring UI widgets",
     home: Scaffold(
       appBar: AppBar(title: Text("Long List"),),
-      body: getListView(),
+      
     ),
   )
   );
 }
 
-//prepare datasource
-//convert datasource into widgets
-//use widgets as children of listview
-
-//prepare datasource
-List<String> getListElements(){
-  var items = List<String>.generate(100, (Counter) => "Item $Counter");
-  return items;
-}
-
-//convert datasource into widgets
-Widget getListView(){
-
-  var listItems = getListElements();
-
-  var listView = ListView.builder(
-    itemBuilder: (context, index) {
-       return ListTile(
-        title: Text(listItems[index]),
-        onTap: (){
-          debugPrint("You tapped $index");
-        },
-       );
-    }
-    
-    );
-    return listView;
-}
