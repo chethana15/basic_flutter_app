9bdaf0bd18b06db1d8ca7ee26e23c26add316ca6
Chethana N P <chethana.np@cumulations.com>
2022-06-26 13:20:09 +0530

    adding rows and columns

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -9,21 +9,78 @@ class Home extends StatelessWidget {
         // width: 300.0,
         // height: 100.0,
       // margin: EdgeInsets.all(20.0),  
-      // margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 0.0, bottom: 0.0),
+      //  margin: EdgeInsets.only(left: 0.0, right: 0.0, top: 40.0, bottom: 0.0),
+      padding: EdgeInsets.only(top: 40.0,left: 20.0),
       alignment: Alignment.center,
       color: Color.fromARGB(255, 208, 126, 246),
-      child: Text(
-        "Flight", 
+      child: Column(
+        children: <Widget>[
+              Row(
+        children: <Widget>[
+        Expanded(child: Text(
+        "Rain", 
         textDirection: TextDirection.ltr,
         style: TextStyle(
           decoration:TextDecoration.none,
-          fontSize: 70.0,
+          fontSize: 25.0,
           fontFamily: 'DancingScript',
           fontWeight: FontWeight.w700, 
-          fontStyle: FontStyle.italic,
           color: Colors.white,
         ), 
         ),
+        ),  
+
+         Expanded(child: Text(
+        "Rain, rain, go away", 
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          decoration:TextDecoration.none,
+          fontSize: 25.0,
+          fontFamily: 'DancingScript',
+          fontWeight: FontWeight.w700, 
+          color: Colors.white,
+        ), 
+        ),
+        ) 
+        ],
+      ),
+           Row(
+        children: <Widget>[
+        Expanded(child: Text(
+        "Rain", 
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          decoration:TextDecoration.none,
+          fontSize: 25.0,
+          fontFamily: 'DancingScript',
+          fontWeight: FontWeight.w700, 
+          color: Colors.white,
+        ), 
+        ),
+        ),  
+
+         Expanded(child: Text(
+        "Rain, rain, go away", 
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          decoration:TextDecoration.none,
+          fontSize: 25.0,
+          fontFamily: 'DancingScript',
+          fontWeight: FontWeight.w700, 
+          color: Colors.white,
+        ), 
+        ),
+        ) 
+        ],
+      )
+        ],
+      )
+      
+      
+ 
+      
+      
+      
     ),
     )
     
