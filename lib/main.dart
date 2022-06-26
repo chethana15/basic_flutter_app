0fa7f374bfcd5a7a38943cf3adece8186cceab56
Chethana N P <chethana.np@cumulations.com>
2022-06-26 22:46:30 +0530

    long list

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import './app_screens/home.dart';
 
@@ -7,40 +10,38 @@ void main(){
     debugShowCheckedModeBanner: false,
     title: "Exploring UI widgets",
     home: Scaffold(
-      appBar: AppBar(title: Text("Learning Lists"),),
+      appBar: AppBar(title: Text("Long List"),),
       body: getListView(),
     ),
   )
   );
 }
 
+//prepare datasource
+//convert datasource into widgets
+//use widgets as children of listview
+
+//prepare datasource
+List<String> getListElements(){
+  var items = List<String>.generate(100, (Counter) => "Item $Counter");
+  return items;
+}
+
+//convert datasource into widgets
 Widget getListView(){
 
-  var listView = ListView(
-    children: <Widget>[
-       
-       ListTile(
-        leading: Icon(Icons.cake),
-        title: Text("Happy Birthday"),
-        subtitle: Text("Have a great day!"),
-        trailing: Icon(Icons.access_time),
-       ),
+  var listItems = getListElements();
 
-        ListTile(
-        leading: Icon(Icons.account_balance_wallet),
-        title: Text("Here's your wallet"),
-        subtitle: Text("Don't spend too much!"),
-        trailing: Icon(Icons.balance),
-       ),
-
-       Text("I'm a text widget"),
-
-      Container(
-        height: 40.0,
-        color: Colors.black,
-      ) 
-    ],
-  );
-
-  return listView;
+  var listView = ListView.builder(
+    itemBuilder: (context, index) {
+       return ListTile(
+        title: Text(listItems[index]),
+        onTap: (){
+          debugPrint("You tapped $index");
+        },
+       );
+    }
+    
+    );
+    return listView;
 }
