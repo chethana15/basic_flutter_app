d1e1009a8f7cf27dc2f9f9a6fec8074f6387dad0
Chethana N P <chethana.np@cumulations.com>
2022-06-26 12:46:27 +0530

    adding custom font

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -12,7 +12,18 @@ class Home extends StatelessWidget {
       // margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 0.0, bottom: 0.0),
       alignment: Alignment.center,
       color: Color.fromARGB(255, 208, 126, 246),
-      child: Text("Flight", textDirection: TextDirection.ltr),
+      child: Text(
+        "Flight", 
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          decoration:TextDecoration.none,
+          fontSize: 70.0,
+          fontFamily: 'DancingScript',
+          fontWeight: FontWeight.w700, 
+          fontStyle: FontStyle.italic,
+          color: Colors.white,
+        ), 
+        ),
     ),
     )
     
