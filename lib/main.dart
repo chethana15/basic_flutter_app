76bd7c8d6a1627d24cf7aabb82b75349044e6b83
Chethana N P <chethana.np@cumulations.com>
2022-06-27 23:22:36 +0530

    created stateful app and commented assets and font in .yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,62 @@
 import 'package:flutter/material.dart';
+
+void main(){
+
+runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Stateful widget app",
+    home: FavCity(),
+  )
+);
+
+}
+
+class FavCity extends StatefulWidget {
+
+  @override
+  State<FavCity> createState() => _FavCityState();
+}
+
+class _FavCityState extends State<FavCity> {
+
+  var cityName;
+
+  @override
+  Widget build(BuildContext context) {
+
+debugPrint("Favourite city widget is created");
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Stateful App Example"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+      child: Column(children: [
+
+        TextField(
+          onSubmitted: (String userInput){
+            setState(() {
+              debugPrint("Set state is called. It now redraws the FavCity widget");
+                cityName = userInput;
+            });
+          
+          },
+        ),
+        Padding(padding: EdgeInsets.all(30.0),
+        child: Text(
+          "$cityName is my favourite city!",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(fontSize: 20.0),
+        ),
+        )
+        
+
+      ]
+      ),
+    )
+    );
+    
+    
+  }
+}
